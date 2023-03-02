d874969a537bb49d778c69029b7a6429647049c8
Eldos <eldos.baktybekov@gmail.com>
2023-03-02 22:40:56 +0600

    :construction: Add models for stocks worksheet

---
+++ src/models/stocks.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+from pydantic import BaseModel
+
+__all__ = (
+    'StocksWorksheetRow',
+    'ParsedStocksWorksheet',
+)
+
+
+class StocksWorksheetRow(BaseModel):
+    shop_name: str
+    warehouse_id: int
+    sku: str
+    stocks_amount: int
+
+
+@dataclass(frozen=True, slots=True)
+class ParsedStocksWorksheet:
+    rows: list[StocksWorksheetRow]
+    error_row_numbers: list[int]
