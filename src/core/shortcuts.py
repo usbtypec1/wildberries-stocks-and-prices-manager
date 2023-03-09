0b410df0e02db852d98db4b232232c682b5920eb
Eldos <eldos.baktybekov@gmail.com>
2023-03-09 09:56:49 +0600

    :truck: Move controller methods

--- src/core/shortcuts.py
+++ src/core/shortcuts.py
@@ -1,17 +1,12 @@
-import collections
 import logging
 import pathlib
-from typing import DefaultDict
 
 import openpyxl
 
-import core.models.wildberries_api
-from core import models
 from core.helpers import grouper
 from core.parser import WorkbookParser
 from core.services.http_clients import closing_wildberries_api_http_client
 from core.services.wildberries_api import WildberriesAPIService
-from core.templates import Template
 
 
 def run(api_key: str, workbook_file_path: str | pathlib.Path):
@@ -33,47 +28,3 @@ def run(api_key: str, workbook_file_path: str | pathlib.Path):
                     wildberries_api_service.update_stocks(warehouse_stocks.warehouse_id, stocks_group)
                 except Exception:
                     logging.exception('Error while updating stocks')
-
-
-def download_prices(api_key: str):
-    with closing_wildberries_api_http_client(api_key=api_key) as http_client:
-        wildberries_api_service = WildberriesAPIService(http_client)
-
-        nomenclature_prices = wildberries_api_service.get_prices(models.QuantityStatus.ANY)
-
-    with Template('./выгрузка цен.xlsx') as template:
-        template.add_prices_rows(nomenclature_prices)
-
-
-def download_stocks(api_key: str):
-    warehouse_id_to_stocks_by_skus: DefaultDict[int, list[
-        core.models.wildberries_api.StocksBySku]] = collections.defaultdict(list)
-    skus: set[str] = set()
-
-    with closing_wildberries_api_http_client(api_key=api_key) as http_client:
-        wildberries_api_service = WildberriesAPIService(http_client)
-        warehouses = wildberries_api_service.get_warehouses()
-
-        for nomenclatures in wildberries_api_service.get_nomenclatures():
-            skus |= {
-                sku
-                for nomenclature in nomenclatures
-                for size in nomenclature['sizes']
-                for sku in size['skus']
-                if sku
-            }
-
-        for skus_group in grouper(skus, n=1000):
-            for warehouse in warehouses:
-                stocks_by_skus = wildberries_api_service.get_stocks_by_skus(warehouse_id=warehouse.id,
-                                                                            skus=skus_group)
-                if stocks_by_skus:
-                    warehouse_id_to_stocks_by_skus[warehouse.id] += stocks_by_skus
-                    break
-
-    warehouses_stocks = [
-        models.WarehouseStocks(warehouse_id=warehouse_id, stocks=stocks_by_skus)
-        for warehouse_id, stocks_by_skus in warehouse_id_to_stocks_by_skus.items()
-    ]
-    with Template('./выгрузка остатков.xlsx') as template:
-        template.add_stocks_rows(warehouses_stocks)
--- src/ui/controller.py
+++ src/ui/controller.py
@@ -1,14 +1,21 @@
-import contextlib
-import pathlib
+import collections
+import os.path
+import sys
+from typing import Any, DefaultDict
 
 import PySimpleGUI as sg
-
-from typing import Any
-
 from openpyxl.utils.exceptions import InvalidFileException
+from rich.console import Console
+from rich.live import Live
+from rich.progress import track
+from rich.table import Table
 
-from core import exceptions
-from core.shortcuts import download_prices, run, download_stocks
+import core.models
+from core import exceptions, models
+from core.helpers import grouper
+from core.services.http_clients import closing_wildberries_api_http_client
+from core.services.wildberries_api import WildberriesAPIService
+from core.shortcuts import run
 from core.templates import Template
 from core.validators import validate_api_key
 
@@ -16,8 +23,11 @@ from core.validators import validate_api_key
 def handle_event(window: sg.Window, event: Any, values: Any) -> None:
     match event:
         case '-GENERATE-TEMPLATE-FILE-':
-            with Template(pathlib.Path(__file__).parent / 'шаблон.xlsx') as template:
+            file_path = os.path.join(os.path.dirname(sys.executable), 'шаблон.xlsx')
+            print(file_path)
+            with Template(file_path) as template:
                 template.save()
+                sg.popup(f'Шаблон сохранен в {str(file_path)}')
 
         case '-DOWNLOAD-STOCKS-':
             api_key: str | None = values.get('-API-KEY-INPUT-')
@@ -71,3 +81,58 @@ def handle_event(window: sg.Window, event: Any, values: Any) -> None:
             else:
                 sg.popup('Данные успешно обновлены')
             window['-START-'].update(disabled=False)
+
+
+def download_stocks(console: Console, api_key: str):
+    warehouse_id_to_stocks_by_skus: DefaultDict[int, list[
+        core.models.wildberries_api.StocksBySku]] = collections.defaultdict(list)
+    skus: set[str] = set()
+
+    with closing_wildberries_api_http_client(api_key=api_key) as http_client:
+        wildberries_api_service = WildberriesAPIService(http_client)
+
+        warehouses = wildberries_api_service.get_warehouses()
+        console.log('Информация о складах загружена')
+        for nomenclatures in track(
+                sequence=wildberries_api_service.get_nomenclatures(),
+                description='Загрузка всех skus...',
+                console=console,
+        ):
+            skus |= {
+                sku
+                for nomenclature in nomenclatures
+                for size in nomenclature['sizes']
+                for sku in size['skus']
+                if sku
+            }
+
+        for skus_group in track(
+                sequence=grouper(skus, n=1000),
+                description='Загрузка остатков по skus',
+                console=console,
+        ):
+            for warehouse in warehouses:
+                stocks_by_skus = wildberries_api_service.get_stocks_by_skus(warehouse_id=warehouse.id,
+                                                                            skus=skus_group)
+                if stocks_by_skus:
+                    warehouse_id_to_stocks_by_skus[warehouse.id] += stocks_by_skus
+                    break
+
+    warehouses_stocks = [
+        models.WarehouseStocks(warehouse_id=warehouse_id, stocks=stocks_by_skus)
+        for warehouse_id, stocks_by_skus in warehouse_id_to_stocks_by_skus.items()
+    ]
+    with Template('./выгрузка остатков.xlsx') as template:
+        template.add_stocks_rows(warehouses_stocks)
+    console.log('Остатки выгружены')
+
+
+def download_prices(console: Console, api_key: str):
+    with closing_wildberries_api_http_client(api_key=api_key) as http_client:
+        wildberries_api_service = WildberriesAPIService(http_client)
+        with console.status('Загрузка цен...', spinner='bouncingBall'):
+            nomenclature_prices = wildberries_api_service.get_prices(models.QuantityStatus.ANY)
+
+    with Template('./выгрузка цен.xlsx') as template:
+        template.add_prices_rows(nomenclature_prices)
+    console.log('Цены выгружены')
