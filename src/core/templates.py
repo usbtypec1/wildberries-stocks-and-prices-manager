d7028669a5c2a7b87eb70a72e12ccecd9edbbebe
Eldos <eldos.baktybekov@gmail.com>
2023-03-14 11:04:18 +0600

    :sparkles: Add functions to generate templates

--- src/core/templates.py
+++ src/core/templates.py
@@ -22,6 +22,30 @@ def generate_template_file(file_path: pathlib.Path) -> None:
         workbook.save(file_path)
 
 
+def generate_prices_report_file(
+        *,
+        file_path: pathlib.Path,
+        categories_prices: Iterable[models.CategoryPrices],
+) -> None:
+    workbook = openpyxl.Workbook(write_only=True)
+
+    with contextlib.closing(workbook):
+        worksheet: Worksheet = workbook.create_sheet('Цены')
+        worksheet.append(('Категория товара', 'nm ID', 'Новая цена', 'Скидка'))
+
+        for category_prices in categories_prices:
+
+            for nomenclature_price in category_prices.nomenclature_prices:
+                worksheet.append((
+                    category_prices.category_name,
+                    nomenclature_price.nomenclature_id,
+                    nomenclature_price.price,
+                    nomenclature_price.discount,
+                ))
+
+        workbook.save(file_path)
+
+
 class Template:
 
     def __init__(self, file_path: str | pathlib.Path):
@@ -50,8 +74,11 @@ class Template:
         self.__prices_worksheet.append(('nm ID', 'Цена', 'Скидка (не указывать)'))
         self.__stocks_worksheet.append(('ID склада', 'sku', 'Количество остатков'))
 
-    def add_prices_rows(self, nomenclature_prices: Iterable[models.NomenclaturePrice]) -> None:
-        for nomenclature_price in nomenclature_prices:
+    def add_prices_rows(
+            self,
+            category_to_nomenclature_prices: dict[str, Iterable[models.NomenclaturePrice]],
+    ) -> None:
+        for category, nomenclature_price in category_to_nomenclature_prices.items():
             self.__prices_worksheet.append(
                 (nomenclature_price.nomenclature_id, nomenclature_price.price, nomenclature_price.discount)
             )
