9ba44cab7ed643920a40ca775d315090fbdc898e
Eldos <eldos.baktybekov@gmail.com>
2023-12-28 22:00:09 +0600

    :coffin: Remove dead code

--- src/core/validators.py
+++ src/core/validators.py
@@ -1,6 +1,4 @@
 from core import exceptions
-from core.services.http_clients import closing_wildberries_api_http_client
-from core.services.wildberries_api import WildberriesAPIService
 
 
 def validate_api_key(api_key: str) -> None:
@@ -8,8 +6,3 @@ def validate_api_key(api_key: str) -> None:
         raise exceptions.ValidationError('Введите API ключ')
     if not api_key.isascii():
         raise exceptions.ValidationError('Неправильный API ключ')
-    # with closing_wildberries_api_http_client(api_key=api_key) as http_client:
-    #     try:
-    #         WildberriesAPIService(http_client).get_warehouses()
-    #     except exceptions.UnauthorizedError:
-    #         raise exceptions.ValidationError('Неправильный API ключ')
