ebed7a9255216f17c4671f85969a2d2911cd7794
Eldos <eldos.baktybekov@gmail.com>
2023-12-29 00:17:04 +0600

    :bug: Fix bug when non-subscriptable collection has been passed

--- src/core/helpers/chunks.py
+++ src/core/helpers/chunks.py
@@ -1,4 +1,4 @@
-from collections.abc import Iterable, Sized
+from collections.abc import Iterable
 
 from typing import TypeVar, Generator
 
@@ -9,8 +9,9 @@ T = TypeVar('T')
 
 def chunkify(
         *,
-        items: Iterable[T] | Sized,
+        items: Iterable[T],
         chunk_size: int,
 ) -> Generator[list[T], None, None]:
+    items = tuple(items)
     for i in range(0, len(items), chunk_size):
         yield items[i:i + chunk_size]
