129e619befdec2f9cdf61e12e8e64a3fe22a65b0
Eldos <eldos.baktybekov@gmail.com>
2023-12-28 21:58:13 +0600

    :recycle: Refactor http client factory
    - Rename factory.
    - Add param type hint.
    - Return specific http client type.

--- src/core/services/http_clients.py
+++ src/core/services/http_clients.py
@@ -2,12 +2,22 @@ import contextlib
 
 import httpx
 
-__all__ = ('closing_wildberries_api_http_client',)
+from core.models.http_clients import WildberriesHTTPClient
+
+__all__ = ('closing_wildberries_http_client',)
 
 
 @contextlib.contextmanager
-def closing_wildberries_api_http_client(*, api_key: str, timeout=120) -> httpx.Client:
+def closing_wildberries_http_client(
+        *,
+        api_key: str,
+        timeout: int = 120,
+) -> WildberriesHTTPClient:
     base_url = 'https://suppliers-api.wildberries.ru/'
     headers = {'Authorization': f'Bearer {api_key}'}
-    with httpx.Client(base_url=base_url, headers=headers, timeout=timeout) as client:
-        yield client
+    with httpx.Client(
+            base_url=base_url,
+            headers=headers,
+            timeout=timeout,
+    ) as http_client:
+        yield WildberriesHTTPClient(http_client)
