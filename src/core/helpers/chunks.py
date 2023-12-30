e5cbb794ba1115b295c2a51a9451b8a75a9e2ba7
Eldos <eldos.baktybekov@gmail.com>
2023-12-30 13:00:55 +0600

    :sparkles: Add utility for counting chunks

--- src/core/helpers/chunks.py
+++ src/core/helpers/chunks.py
@@ -1,8 +1,8 @@
 from collections.abc import Iterable
 
-from typing import TypeVar, Generator
+from typing import TypeVar, Generator, Any
 
-__all__ = ('chunkify',)
+__all__ = ('chunkify', 'count_chunks')
 
 T = TypeVar('T')
 
@@ -15,3 +15,13 @@ def chunkify(
     items = tuple(items)
     for i in range(0, len(items), chunk_size):
         yield items[i:i + chunk_size]
+
+
+def count_chunks(
+        *,
+        items: Iterable[Any],
+        chunk_size: int,
+) -> int:
+    items = tuple(items)
+    items_count = len(items)
+    return items_count // chunk_size + ((items_count % chunk_size) > 0)
